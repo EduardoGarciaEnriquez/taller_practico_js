6e5b590500c88381d11b1f709be0527e999f654c
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2024-04-16 13:55:49 -0600

    feat: move player

--- main.js
+++ main.js
@@ -2,9 +2,59 @@ const canvas = document.querySelector("#game");
 const game = canvas.getContext("2d");
 let canvasSize;
 let elementSize;
+const playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 window.addEventListener("load", resizeCanvas);
 window.addEventListener("resize", resizeCanvas);
+
+window.addEventListener("keyup", (event) => move(event.code));
+
+up?.addEventListener("click", () => move("ArrowUp"));
+left?.addEventListener("click", () => move("ArrowLeft"));
+right?.addEventListener("click", () => move("ArrowRight"));
+down?.addEventListener("click", () => move("ArrowDown"));
+
+function move(direction) {
+  if (direction === "ArrowUp") moveUp();
+  else if (direction === "ArrowDown") moveDown();
+  else if (direction === "ArrowLeft") moveLeft();
+  else if (direction === "ArrowRight") moveRight();
+}
+
+function moveUp() {
+  if (playerPosition.y > 1) {
+    playerPosition.y--;
+  }
+  resizeCanvas();
+  paintPlayer();
+}
+
+function moveDown() {
+  if (playerPosition.y <= 9) {
+    playerPosition.y++;
+  }
+  resizeCanvas();
+  paintPlayer();
+}
+
+function moveLeft() {
+  if (playerPosition.x > 0) {
+    playerPosition.x--;
+  }
+  resizeCanvas();
+  paintPlayer();
+}
+
+function moveRight() {
+  if (playerPosition.x < 9) {
+    playerPosition.x++;
+  }
+  resizeCanvas();
+  paintPlayer();
+}
 
 function paintCanvas() {
   const map = maps[0]
@@ -21,8 +71,24 @@ function paintCanvas() {
         elementSize * col,
         elementSize * row
       );
+
+      if (map[row - 1][col] === "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = col;
+          playerPosition.y = row;
+          paintPlayer();
+        }
+      }
     }
   }
+}
+
+function paintPlayer() {
+  game.fillText(
+    emojis["PLAYER"],
+    elementSize * playerPosition.x,
+    elementSize * playerPosition.y
+  );
 }
 
 function resizeCanvas() {
@@ -41,7 +107,6 @@ function resizeCanvas() {
 
   canvas.setAttribute("width", canvasSize);
   canvas.setAttribute("height", canvasSize);
-
   elementSize = canvasSize / 10 - 1;
 
   paintCanvas();
